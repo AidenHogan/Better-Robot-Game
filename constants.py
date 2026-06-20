006a6ad279b0f3145abd18e3e4e51abb84340a4a
AidenHogan <amelioratedMail@gmail.com>
2026-06-20 00:09:08 -0700

    feat(constants): Add tile height ranges
    Early set up for procedural terrain generation with z-levels

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # Contains most of the game settings and other immutable characteristics
-import arcade #Game library
+import arcade # Game library
 
 #Window Settings
 WINDOW_WIDTH = 1280
@@ -9,8 +9,8 @@ WINDOW_TITLE = "Robotica"
 #World generation settings
 TILE_SIZE = 40
 MAP_SIZE = 20
-SURFACE_LEVEL = 2
-Z_LEVELS = 5 #0 is the highest sky level.  The number here is the lowest (undeground). SURFACE_LEVEL is set above
+SURFACE_LEVEL = 50
+Z_LEVELS = 100 #0 is the highest sky level.  The number here is the lowest (undeground). SURFACE_LEVEL is set above
 
 
 # Define colors and tile types for the tile map
@@ -21,3 +21,21 @@ TILE_COLORS = {
     "stone": arcade.color.ASH_GREY,
     "sky": [135, 206, 250, 15] #Sky blue, opacity 125 = 50%, 0 = invisible, 250 = 100%
     }
+
+# Defines areas which will determine what tile types can spawn where
+# For 100 levels: First 20 are sky, Next 10 are Mountain layers
+# Next 5 are Woodland layers, Next two are Grasslands, Next is Marsh
+# Next is Beach trees, Next two are Dunes, Next is Beach
+# Next 10 are water/ocean layers, The rest is underground
+TILE_HEIGHT_RANGES = {
+    "sky": (0,20),
+    "mountain": (20, 30),
+    "woodlands": (30, 35),
+    "grasslands": (35, 37),
+    "marsh": (37, 37),
+    "beach_trees": (38, 38),
+    "dunes": (38, 40),
+    "beach": (40, 40),
+    "ocean": (40, 50),
+    "underground": (50, 100)
+}
